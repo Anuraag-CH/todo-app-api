24a206d8f8b2a4d07eee8310f0c6c748e50aba67
Anuraag-CH <anuraag.ch@gmail.com>
2023-08-03 00:12:46 +0530

    add put

--- todoserver.js
+++ todoserver.js
@@ -1,62 +1,80 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const todoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    completed: { type: Boolean, default: false }
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
-const Todo = mongoose.model('Todo', todoSchema)
+const Todo = mongoose.model('Todo', todoSchema);
 
-
-// connect to mongo
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect('mongodb://127.0.0.1:27017/todos', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/todos', async (req, res) => {
-    // list all mongo details of todo
-
-    const todos = await Todo.find()
-    res.json(todos)
-
-})
+    const todos = await Todo.find();
+    res.json(todos);
+});
 
 app.get('/todos/:id', async (req, res) => {
-    const { id } = req.params
-    const todo = await Todo.findOne({ _id: id })
-    res.json(todo)
-
-})
+    const { id } = req.params;
+    const todo = await Todo.findOne({ _id: id });
+    res.json(todo);
+});
 
 app.post('/todos', async (req, res) => {
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.status(201).send(todo.id);
+});
 
-    const todo = new Todo(req.body)
-    await todo.save()
-    res.status(201).send(todo.id)
+app.put('/todos/:id', async (req, res) => {
 
-})
+    try {
+        const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        if (todo) {
+            res.send("Todo Updated");
+        }
+        else {
+            res.sendStatus(404)
+        }
+    }
+    catch (error) {
+        res.sendStatus(404)
+    }
+
+});
 
 app.delete('/todos/:id', async (req, res) => {
-
-    const todo = await Todo.findByIdAndDelete(req.params.id)
-
+    const todo = await Todo.findByIdAndDelete(req.params.id);
     if (todo) {
-        res.send("Todo deleted")
+        res.send("Todo deleted");
+    } else {
+        res.status(404).send("Todo Not found");
     }
-    else {
-        res.status(404).send("Todo Not found")
-    }
-
-})
+});
 
 app.use((req, res, next) => {
     res.status(404).send();
 });
 
-
-app.listen(3000, () => (console.log("listening on port 3000")))
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
